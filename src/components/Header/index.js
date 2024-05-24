666656c96c2304372c535fbf78c9b8760916be6e
vamsimohanyacham <142957313+vamsimohanyacham@users.noreply.github.com>
2024-05-24 13:04:42 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,7 +18,7 @@ const Header = props => {
           <div className="nav-bar-mobile-container">
             <Link to="/">
               <img
-                src="https://assets.ccbp.in/frontend/react-js/logo-img.png "
+                src="https://th.bing.com/th/id/R.240f34355ff6a18800af9581f6b5eeab?rik=5HHG75UX2kjcBA&riu=http%3a%2f%2fbrand.opportunity.org%2fimages%2fmark3_2.png&ehk=i4aqQT2nMo9g66YxQUEoahQFMm7IIEvb7dDgNx91LA4%3d&risl=&pid=ImgRaw&r=0"
                 className="website-logo"
                 alt="website logo"
               />
@@ -51,7 +51,7 @@ const Header = props => {
             <Link to="/">
               <img
                 className="website-logo"
-                src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+                src="https://th.bing.com/th/id/R.240f34355ff6a18800af9581f6b5eeab?rik=5HHG75UX2kjcBA&riu=http%3a%2f%2fbrand.opportunity.org%2fimages%2fmark3_2.png&ehk=i4aqQT2nMo9g66YxQUEoahQFMm7IIEvb7dDgNx91LA4%3d&risl=&pid=ImgRaw&r=0"
                 alt="website logo"
               />
             </Link>
